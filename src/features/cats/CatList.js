727fb81c339e603f91413ebbd7ecea7e4db7f4a2
Alessandra Chardon <achardon@ucla.edu>
2022-04-29 12:02:47 -0400

    commit tree 1f30956ca5da7afc292bab4f4c62a4d347835101

--- src/features/cats/CatList.js
+++ src/features/cats/CatList.js
@@ -1,1 +1,11 @@
-// write your CatList component here
+export default function CatList( {catPics} ) {
+    return(
+        <div>
+            {catPics? 
+            catPics.map(cat => {
+                return <img key={cat.id} src={cat.url} alt="cat" height="300" />
+            }) :
+            null }
+        </div>
+    )
+}
